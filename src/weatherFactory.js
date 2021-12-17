2044f9c2c24bfbc41e095afa2060cbe2bbdf7c6c
Raza Hassan <bilgramiraza@yahoo.com>
2021-12-17 13:26:35 +0530

    Centrelized system for Tracking data

---
+++ src/weatherFactory.js
@@ -0,0 +1,63 @@
+class Weather{
+    constructor(){
+        this.lat = null;
+        this.lon = null;
+        this.location = null;
+        this._currentWeather = {};
+        this._hourly = [{}];
+        this._daily = [{}];
+    }
+    set currentWeather(data){
+        this._currentWeather = data;
+    }
+    set hourly(data){
+        data.forEach(item => this._hourly.push(item));
+    }
+    set daily(data){
+        data.forEach(item => this._daily.push(item));
+    }
+    setLocationData(lat, lon, location){
+        this.lat = lat;
+        this.lon = lon;
+        this.location = location;
+    }
+    getCoords(){
+        if(this.lat || this.lon)
+            return [this.lat, 
+                    this.lon];
+        else
+            return null;
+    }
+    getLocation(){
+        if(this.location)
+            return this.location;
+        else
+            return null;
+    }
+    getCurrentWeather(){
+        if(Object.keys(this.currentWeather).length)
+            return this.currentWeather;
+        else
+            return null;
+    }
+    getHourlyForcast(){
+        if(Object.keys(this.hourly).length)
+            return this.hourly;
+        else
+            return null;
+    }
+    getWeeklyForcast(){
+        if(Object.keys(this.daily).length)
+            return this.daily;
+        else
+            return null;
+    }
+    isLocationSet(){
+        if(this.lat !== null && this.lon !== null)
+            return true;
+        else
+            return false;
+    }
+}
+
+export default Weather;

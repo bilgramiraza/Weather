788d223206fa34b1ca2b0166dbb7be9eb8a0827c
Raza Hassan <bilgramiraza@yahoo.com>
2021-12-18 12:20:17 +0530

    built files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import './CSS/style.css';
 import eventHandlers from './SiteBuilder/eventHandlers';
 import {buildErrorDOM} from './ErrorHandling/errorHandling';
 import Weather from './weatherFactory';
-// const weatherCall = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=17.3850&lon=78.4867&exclude=minutely,current&appid=${APIKey}`,{mode:'cors'});
 
 let weather = new Weather();
 eventHandlers(weather);

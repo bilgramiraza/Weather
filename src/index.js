497cb3bf17cc94feeae5ea7f38b3b5ca8b32cafc
Raza Hassan <bilgramiraza@yahoo.com>
2021-12-18 12:11:40 +0530

    Implemented Weather Class

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import './CSS/reset.css';
 import './CSS/style.css';
 import eventHandlers from './SiteBuilder/eventHandlers';
 import {buildErrorDOM} from './ErrorHandling/errorHandling';
+import Weather from './weatherFactory';
 // const weatherCall = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=17.3850&lon=78.4867&exclude=minutely,current&appid=${APIKey}`,{mode:'cors'});
 
-
-eventHandlers();
+let weather = new Weather();
+eventHandlers(weather);
 buildErrorDOM();

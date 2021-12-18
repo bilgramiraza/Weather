64c04d62216da717b64ff8c89d486b3d03744734
Raza Hassan <bilgramiraza@yahoo.com>
2021-12-18 12:15:02 +0530

    Added Forecast Button

--- src/SiteBuilder/eventHandlers.js
+++ src/SiteBuilder/eventHandlers.js
@@ -1,11 +1,21 @@
-import { weatherAPI } from '../WeatherAPI';
+import { weatherAPI, forecastAPI } from '../WeatherAPI';
 import { display } from '../SiteBuilder/display';
 
-function eventHandlers() {
+function eventHandlers(weather) {
     document.body.addEventListener("click",(event)=>{
         switch (event.target.className) {
             case "locationBtn":
-                app();
+                forecast(weather);
+                break;
+            case "forecastBtn":
+                if(weather.isLocationSet()){
+                    document.querySelector(".forecast").classList.toggle("hide");
+                    document.querySelector(".forecastBtn").classList.toggle("hide");
+                    extendedForecast(weather);
+                }
+                else{
+                    alert("Enter a Valid location");
+                }
                 break;
         }
     });
@@ -17,8 +27,20 @@ function eventHandlers() {
 
 export default eventHandlers;
 
-async function app() {
+async function forecast(weather) {
     const seachBar = document.querySelector("#searchBar");
     const data = await weatherAPI(seachBar.value);
-    display(data);
+    if(data !== null){
+        weather.setLocationData(data.lat, data.lon, data.location);
+        weather.currentWeather = data;
+        display(data);
+    }
+}
+async function extendedForecast(weather) {
+    const [hourlyForecast, dailyForecast] = await forecastAPI(...weather.getCoords());
+    if(hourlyForecast !== null || dailyForecast !== null ){
+        weather.hourly = hourlyForecast;
+        weather.daily = dailyForecast;
+        console.log(weather);
+    }
 }
